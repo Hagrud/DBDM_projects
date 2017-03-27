8fdd5b2d4dd2f8c4a6ac4cf293c5250a8f136798
hagrud <pep.polet@gmail.com>
2017-03-27 15:03:07 +0200

    perf.sh add

--- perf.sh
+++ perf.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 GEN_CMD="java -jar closure.jar -generate"
-NLIN_CL_CMD="java -jar closure.jar -naive - '0' >/dev/null"
-LIN_CL_CMD="java -jar closure.jar -improved - '0' >/dev/null"
+NLIN_CL_CMD="java -jar closure.jar -naive temp.txt 0 >/dev/null"
+LIN_CL_CMD="java -jar closure.jar -improved temp.txt 0 >/dev/null"
 
 TIMEFORMAT=%R;
 echo -e "# \t nlin \t lin" 
 n=10;
 for i in `seq 100 100 1000`; do
   for j in `seq 1 $n`; do
-      echo -e $GEN_CMD $i | $NLIN_CL_CMD
-      nlin_time=$( $GEN_CMD $i | { time  $NLIN_CL_CMD > /dev/null; } 2>&1)
-	  lin_time=$( $GEN_CMD $i | { time $LIN_CL_CMD > /dev/null; } 2>&1)
+      $GEN_CMD $i > "temp.txt"
+      nlin_time=$({ time  $NLIN_CL_CMD > /dev/null; } 2>&1)
+	  lin_time=$({ time $LIN_CL_CMD > /dev/null; } 2>&1)
     echo -e "$i \t $nlin_time \t $lin_time" 
   done
 done
